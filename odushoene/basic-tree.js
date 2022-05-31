c69f5f7353a21bbaa483ab239e5bcf30fbe7615c
rsnelson23 <rsnelson23@gmail.com>
2022-05-31 01:39:00 -0400

    Data Fix

--- odushoene/basic-tree.js
+++ odushoene/basic-tree.js
@@ -14,7 +14,7 @@ fetch("./data.json").then(r => r.json()).then(data => {
       store,
       svg: view.svg,
       card_dim: {w:220,h:70,text_x:75,text_y:15,img_w:60,img_h:60,img_x:5,img_y:5},
-      card_display: [d => d.data.label || '', d => d.data.desc || ''],
+      card_display: [d => `${d.data["first name"]} ${d.data["maiden name"]} ${d.data["last name"]}`,d => `${d.data["birthday"]}`],
       mini_tree: true,
       link_break: false
     })
